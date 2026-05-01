2f4dcc5a0bb0d65b4c472bc1114ee408e6a08f21
samaly2125-crypto <samaly.2125@gmail.com>
2026-04-30 19:18:05 -0500

    gfdd

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("mysql-marcela.alwaysdata.net", "marcela", "clase1234", "marcela_envios");
+$conexion = new mysqli("mysql-marcela.alwaysdata.net", "marcela", "lina123.", "marcela_envios");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
